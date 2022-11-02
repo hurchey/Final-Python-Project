da1361477ef434f6bf454f0ebb2961e41036ca4e
hurchey <117241771+hurchey@users.noreply.github.com>
2022-11-01 21:46:55 -0400

    Add files via upload

---
+++ eric_hurchey PA1.py
@@ -0,0 +1,38 @@
+def triple(word):
+    print(word, word, word)
+def draw_paragraph():
+    print("WE ARE MIGHTY BRANDEIS")
+    print("BLEEDING BLUE & WHITE")
+    print("WE CAME TO \"BANG THE GRAVEL\"")
+    triple("FIGHT!")
+    print()
+def draw_paragraph2():
+    print("THERE ARE NO CURLY BRACES")
+    print("WE \\INDENT \\TO \\THE \\RIGHT")
+    print("WE LEARN TO CODE IN PYTHON")
+    triple("WRITE!")
+    print()
+def draw_paragraph3():
+    print("WE TRY TO BE EFFICIENT")
+    print("WE PROGRAM THRU THE NIGHT")
+    print("BUT \tWE \tUSED \tTOO \tMUCH \tMEMORY \t#use \\t tabs")
+    triple("TIGHT!")
+    print()
+def draw_paragraph4():
+    print("BRANDEIS COMPUTER SCIENCE")
+    print("LEADS US TO THE LIGHT")
+    print("OUR PROGRAM IS \"CORRECT\"")
+    triple("RIGHT!")
+    print()
+def draw_paragraph5():
+    print("LOUIS D. BRANDEIS")
+    print("HE WAS TRULY BRIGHT")
+    print("WE NEVER BACK DOWN CAUSE WE")
+    triple("FIGHT!")
+def main():
+    draw_paragraph()
+    draw_paragraph2()
+    draw_paragraph3()
+    draw_paragraph4()
+    draw_paragraph5()
+main()
