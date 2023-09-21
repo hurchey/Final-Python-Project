3a827d31adcd7bbd9cf3d99112ab3c495a4e31f8
hurchey <117241771+hurchey@users.noreply.github.com>
2023-09-21 07:54:09 -0400

    Add files via upload

---
+++ ca01/gpt.py
@@ -0,0 +1,87 @@
+'''
+Demo code for interacting with GPT-3 in Python.
+
+To run this you need to 
+* first visit openai.com and get an APIkey, 
+* which you export into the environment as shown in the shell code below.
+* next create a folder and put this file in the folder as gpt.py
+* finally run the following commands in that folder
+
+On Mac
+% pip3 install openai
+% export APIKEY="......."  # in bash
+% python3 gpt.py
+
+On Windows:
+% pip install openai
+% $env:APIKEY="....." # in powershell
+% python gpt.py
+'''
+import openai
+
+class GPT():
+
+
+    ''' make queries to gpt from a given API '''
+    def __init__(self,apikey):
+        ''' store the apikey in an instance variable '''
+        self.apikey=apikey
+        # Set up the OpenAI API client
+        openai.api_key = apikey #os.environ.get('APIKEY')
+
+        # Set up the model and prompt
+        self.model_engine = "text-davinci-003"
+
+    def result(self, self_obj, prompt):
+        '''Ming: I added this function that returns the results to avoid writing this so many times in our program'''
+        completion = openai.Completion.create(
+            engine=self_obj.model_engine,
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+        return completion
+    
+    def getResponse(self,prompt):
+        ''' Harry - template to generate a GPT response '''
+        completion = self.result(self,prompt)
+        response = completion.choices[0].text
+        return response
+    
+    def recipe(self,course):
+        ''' Generate a GPT response '''
+        prompt = 'genrate a recipe for ' + course
+        completion = self.result(self, prompt)
+        response = completion.choices[0].text
+        return response 
+    
+    def get_tools_for_recipe(self, course):
+        '''Ming'''
+        '''generates a GPT response that tells the user what they should get to complete the course they are cooking'''
+        prompt = f'what are the things I would need to cook {course} as a college student'
+        completion = self.result(self, prompt)
+        response = completion.choices[0].text
+        return response 
+    
+    #Xiaoran's prompt of getting what day is it today!
+    def get_celebrate(self, date):
+        prompt = f'What day is celebrated on {date}?'
+        completion = self.result(self, prompt)
+        response = completion.choices[0].text
+        return response
+        
+    def get_birthday_of_president(day, president):
+        '''Eric'''
+        prompt = "What is the birhday of " + president + " ?"
+        completion = day.result(day, prompt)
+        response = completion.choices[0].text
+        return response
+
+if __name__=='__main__':
+    '''
+    '''
+    import os
+    g = GPT(os.environ.get("APIKEY"))
+    print(g.recipe("chicken"))
---
+++ ca01/gptwebapp.py
@@ -0,0 +1,243 @@
+'''
+gptwebapp shows how to create a web app which ask the user for a prompt
+and then sends it to openai's GPT API to get a response. You can use this
+as your own GPT interface and not have to go through openai's web pages.
+
+We assume that the APIKEY has been put into the shell environment.
+Run this server as follows:
+
+On Mac
+% pip3 install openai
+% pip3 install flask
+% export APIKEY="......."  # in bash
+% python3 gptwebapp.py
+
+On Windows:
+% pip install openai
+% pip install flask
+% $env:APIKEY="....." # in powershell
+% python gptwebapp.py
+'''
+from flask import request,redirect,url_for,Flask
+from gpt import GPT
+import os
+
+app = Flask(__name__)
+gptAPI = GPT(os.environ.get('APIKEY'))
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
+
+# This is the nav_bar for all pages 
+nav_bar=f'''
+    <head>
+        <link rel= "stylesheet" type= "text/css" href= "static/css/styles.css">
+    </head>
+    <div class="nav-bar">
+        <h1 class="team-name">GPT Project Team 14</h1>
+        <a href="/">Home</a>
+        <a href="/about">About Page</a> 
+        <a href="/team">Team Page</a>
+        <a href="/ming">Ming-Shih</a>
+        <a href="/xiaoran">Xiaoran</a>
+        <a href="/harry">Harry</a>
+    </div>
+    '''
+
+@app.route('/')
+def index():
+    ''' display a link to the general query page '''
+    print('processing / route')
+    return f'''
+    {nav_bar}
+    <div class="container"><h1>Welcome to Team 14's project!</h1></div>
+    '''
+
+@app.route('/team')
+def team():
+    return f'''
+    {nav_bar}
+    <div class="container">
+        <div class="item">
+            <h1>Xiaoran</h2>
+            <ul>
+                <li>Major: Computer Science</li>
+                <li>Class standing: first-year master student</li>
+                <li>Hometown: Beijing, China</li>
+                <li>Role: Software Development and Emotional Support Engineer</li>
+                <li>Fun fact: I was in a plane accident at the age of 12!</li>
+            </ul>
+        </div>
+        <div class="item">
+            <h1>Harry</h2>
+            <ul>
+                <li>Major: Computer Science & Anthropology</li>
+                <li>Class standing: sophomore</li>
+                <li>Hometown: Beijing, China</li>
+                <li>Role: Software Development</li>
+                <li>Fun fact: I love the color yellow, yea...</li>
+            </ul>
+        </div>
+        <div class="item">
+            <h1>Ming</h2>
+            <ul>
+                <li>Major: Computer Science & Applied Mathematics</li>
+                <li>Class standing: sophomore</li>
+                <li>Hometown: Taiwan</li>
+                <li>Role: Created the nav bar for the website and styled the website a bit</li>
+                <li>Fun fact: I love basketball, and I don't think it's a "fun" fact Xiaoran LOL</li>
+            </ul>
+        </div>
+    </div>
+    '''
+@app.route('/about')
+def about():
+    return f'''
+    {nav_bar}
+    <div class="aboutContainer">
+        <h1>What does our program do?</h1>
+        <div class="item">
+            <h2>Ming</h2>
+            <p>My program generates a GPT response that tells the user what they should get to complete the course they are cooking.</p>
+        </div>
+        <div class="item">
+            <h2>Xiaoran</h2>
+            <p>My program asks the user to enter a date of their choice and return a holiday/national day of that specific date via ChatGPT openai API!</p>
+        </div>
+         <div class="item">
+            <h2>Harry</h2>
+            <p>My function provides a recipe for the course my user is looking fo. Eat well!</p>
+        </div>
+        <div class = "item">
+            <h2>Eric</h2>
+            <p>My program asks the user to enter a president of their choice and then the program will return the birthday of that president in question</p>
+        </div>
+    </div>
+    '''
+@app.route('/ming', methods=['GET', 'POST'])
+def ming():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.get_tools_for_recipe(prompt)
+        return f'''
+        {nav_bar}
+        <div class="answer">
+            <h2>{answer}</h2>
+        </div>
+        '''
+    else:
+        return f'''
+        {nav_bar}
+        <div class="container">
+            <h1>Ming-Shih</h1>
+            <h2>Enter the course you're cooking and recieve a list of tools you'll need</h2>
+            <form class="form" method="post">
+                <textarea name="prompt"></textarea>
+                <p><input class="submit" type=submit value="get response"><p/>
+            </form>
+        </div>
+        '''
+    
+@app.route('/xiaoran', methods=['GET', 'POST'])
+def xiaoran():
+    if request.method == 'POST':
+        date = request.form['date']
+        answer = gptAPI.get_celebrate(date)
+        return f'''
+        {nav_bar}
+        <div class="answer">
+            <h2>{answer}</h2>
+        </div>
+        '''
+    else:
+        return f'''
+        {nav_bar}
+        <div class="container">
+            <h1>Xiaoran</h1>
+            <h2>What day is celebrated on </h2>
+            <form class="form" method="post">
+                <textarea name="date"></textarea>
+                <p><input class="submit" type=submit value="get response"></p>
+            </form>
+        </div>
+        '''
+    
+@app.route('/harry', methods=['GET', 'POST'])
+def harry():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        answer = gptAPI.recipe(prompt)
+        return f'''
+        {nav_bar}
+        <div class="answer">
+            <h2>{answer}</h2>
+        </div>
+        '''
+    else:
+        return f'''
+        {nav_bar}
+        <div class="container">
+            <h1>Harry C.</h1>
+            <h2>Enter the course you're cooking tonight and you will receive the recipe for it</h2>
+            <form class="form" method="post">
+                <textarea name="prompt"></textarea>
+                <p><input class="submit" type=submit value="get response"><p/>
+            </form>
+        </div>
+        '''
+    
+    @app.route('/eric', methods = ['GET', 'POST'])
+    def eric():
+        if request.method == 'POST':
+            prompt = request.form['prompt']
+            answer = gptAPI.recipe(prompt)
+            return f'''
+            {nav_bar}
+            <div class="answer">
+                <h2>{answer}</h2>
+            </div>
+            '''
+        else:
+            return f'''
+            {nav_bar}
+            <div class="container">
+            <h1>Eric Hurchey</h1>
+            <h2>Enter the president you're wanting to know the birthday for</h2>
+            <form class="form" method="post">
+                <textarea name="prompt"></textarea>
+                <p><input class="submit" type=submit value="get response"><p/>
+            </form>
+        </div>
+        '''
+
+# @app.route('/gptdemo', methods=['GET', 'POST'])
+# def gptdemo():
+#     ''' handle a get request by sending a form 
+#         and a post request by returning the GPT response
+#     '''
+#     if request.method == 'POST':
+#         prompt = request.form['prompt']
+#         answer = gptAPI.getResponse(prompt)
+#         return f'''
+#         <h1>GPT Demo</h1>
+#         <pre style="bgcolor:yellow">{prompt}</pre>
+#         <hr>
+#         Here is the answer in text mode:
+#         <div style="border:thin solid black">{answer}</div>
+#         Here is the answer in "pre" mode:
+#         <pre style="border:thin solid black">{answer}</pre>
+#         <a href={url_for('gptdemo')}> make another query</a>
+#         '''
+#     else:
+#         return '''
+#         <h1>GPT Demo App</h1>
+#         Enter your query below
+#         <form method="post">
+#             <textarea name="prompt"></textarea>
+#             <p><input type=submit value="get response">
+#         </form>
+#         '''
+
+if __name__=='__main__':
+    # run the code on port 5001, MacOS uses port 5000 for its own service :(
+    app.run(debug=True,port=5001)
